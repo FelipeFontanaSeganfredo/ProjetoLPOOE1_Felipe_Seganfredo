0a4eaee9c5ce78678c28284d14ecb03e33828085
FelipeFontanaSeganfredo <felipefseganfredo@gmail.com>
2024-11-04 14:21:51 -0300

    Início do projeto

---
+++ Transportadora/src/main/java/com/mycompany/transportadora/Transportadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.transportadora;
+
+/**
+ *
+ * @author User PC
+ */
+public class Transportadora {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
